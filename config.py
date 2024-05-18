49276d930c70e35be99df03c4deece319788f5ba
Radhaxmusic07 <prabhakarnikam0311@Yahoo.com>
2024-05-18 22:55:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,8 +38,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Channel_Club")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Time_Pass_420")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LOVE03_14")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/crazy_chatting_world")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -77,10 +77,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/389ef29df95e5dde2f305.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/0bb2f00d8ff390c495bbf.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/bc7f8903e60fbfafa2eb4.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/0bb2f00d8ff390c495bbf.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://graph.org/file/2fd73ef39c62047594009.jpg"
